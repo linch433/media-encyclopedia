28337dbd428af93471a230517213202601470682
Volodymyr Kopynets <vovakopinecz@gmail.com>
2023-03-30 23:02:30 +0300

    Integrate tailwindcss Integrate default prettier Add basic folder structure

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-red-300 text-3xl font-semibold">
+      Home Page
+    </div>
+  );
+};
+
+export default HomePage;
