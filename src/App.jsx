5dae406e3c70aca33db6bb0838c9c0e15ca7fc7e
Volodymyr Kopynets <vovakopinecz@gmail.com>
2023-03-31 00:12:29 +0300

    Update configurations

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import routes from './routes/index.jsx';
+import { useRoutes } from 'react-router-dom';
+
+const App = () => {
+  const element = useRoutes(routes);
+
+  return <div> {element} </div>;
+};
+
+export default App;
