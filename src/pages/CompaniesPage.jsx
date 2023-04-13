5ad169a57913e635e0997bea4e380f88a9e1eedc
Volodymyr Kopynets <linch433@gmail.com>
2023-04-13 10:52:56 +0300

    Add template info for every page on web-app

--- src/pages/CompaniesPage.jsx
+++ src/pages/CompaniesPage.jsx
@@ -1,10 +1,78 @@
-import React from 'react';
+import {Typography} from "antd";
+import TitleDivider from "../components/VideoGamesHistoryPage/TitleDivider.jsx";
+
+const {Title, Paragraph} = Typography;
 
 const CompaniesPage = () => {
   return (
-    <div>
-      CompaniesPage
-    </div>
+    <Typography>
+      <Title>Ігрові компанії</Title>
+      <Paragraph>
+        "Відомі компанії відеоігор" - це перелік компаній, що займаються розробкою та виданням відеоігор. Індустрія
+        відеоігор є однією з найбільш прибуткових та швидкорозвиваючихся галузей в світі розваг. У цьому переліку
+        включені компанії з різних країн світу, від яких залежить значна частина відеоігрової індустрії.
+      </Paragraph>
+      <TitleDivider title={'Activision Blizzard'}/>
+      <Paragraph>
+        Однією з найвідоміших компаній відеоігор є американська компанія Activision Blizzard. Її найвідоміші ігри
+        включають Call of Duty, World of Warcraft, Diablo та Overwatch. Компанія також є власником ряду студій, таких як
+        Treyarch, Infinity Ward та Blizzard Entertainment.
+      </Paragraph>
+      <TitleDivider title={'Electronic Arts'}/>
+      <Paragraph>
+        Electronic Arts (EA) - ще одна відома американська компанія відеоігор. EA відома своїми іграми в жанрах спорту,
+        перегонів та шутерів від першої особи. До найвідоміших її ігор належать серії FIFA, Madden NFL, Need for Speed
+        та Battlefield.
+      </Paragraph>
+      <TitleDivider title={'Nintendo'}/>
+      <Paragraph>
+        Інша відома компанія відеоігор - японська Nintendo. Вона відома своїми іграми для портативних та домашніх
+        ігрових консолей, таких як Nintendo Switch та Nintendo 3DS. До найвідоміших її ігор належать серії Super Mario,
+        The Legend of Zelda та Pokemon.
+      </Paragraph>
+      <TitleDivider title={'Ubisoft'}/>
+      <Paragraph>
+        Ubisoft - французька компанія, заснована у 1986 році. Вона відома своїми іграми в жанрах екшн, рольових ігор та
+        шутерів. До найвідоміших ігор компанії належать серії Assassin's Creed, Far Cry та Tom Clancy's Rainbow Six.
+      </Paragraph>
+      <TitleDivider title={'Rockstar Games'}/>
+      <Paragraph>
+        Rockstar Games - американська компанія, заснована у 1998 році. Вона відома своїми іграми в жанрах екшн та
+        відкритого світу. До найвідоміших її ігор належать серії Grand Theft Auto та Red Dead Redemption.
+      </Paragraph>
+      <TitleDivider title={'Bethesda Softworks'}/>
+      <Paragraph>
+        Bethesda Softworks - американська компанія, заснована у 1986 році. Вона відома своїми іграми в жанрах рольових
+        ігор та відкритого світу. До найвідоміших ігор компанії належать серії The Elder Scrolls та Fallout.
+      </Paragraph>
+      <TitleDivider title={'Square Enix'}/>
+      <Paragraph>
+        Square Enix - є японською компанією, яка займається розробкою та виданням відеоігор. Вона відома своїми іграми в
+        жанрі рольових ігор та пригодницьких ігор. До найвідоміших ігор компанії належать Final Fantasy, Dragon Quest та
+        Tomb Raider.
+      </Paragraph>
+      <TitleDivider title={'Naughty Dog'}/>
+      <Paragraph>
+        Naughty Dog - це американська компанія, заснована у 1984 році. Вона відома своїми іграми в жанрі пригодницьких
+        ігор та екшн. До найвідоміших ігор компанії належать серії Uncharted та The Last of Us.
+      </Paragraph>
+      <TitleDivider title={'Valve Corporation'}/>
+      <Paragraph>
+        Valve Corporation - американська компанія, заснована у 1996 році. Вона відома своїми іграми та платформою Steam.
+        До найвідоміших ігор компанії належать серії Half-Life, Portal та Counter-Strike.
+      </Paragraph>
+      <TitleDivider title={'CD Projekt Red'}/>
+      <Paragraph>
+        CD Projekt Red - польська компанія, заснована у 1994 році. Вона відома своїми рольовими іграми та відкритими
+        світами. До найвідоміших ігор компанії належать серії The Witcher та Cyberpunk 2077.
+      </Paragraph>
+      <TitleDivider title={'Bioware'}/>
+      <Paragraph>
+        Bioware - канадська компанія, заснована у 1995 році. Вона відома своїми рольовими іграми з акцентом на глибокий
+        сюжет та характеризацію персонажів. До найвідоміших ігор компанії належать серії Baldur's Gate, Mass Effect та
+        Dragon Age.
+      </Paragraph>
+    </Typography>
   );
 };
 
