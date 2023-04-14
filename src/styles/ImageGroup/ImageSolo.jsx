7169531b9dec19b0eaae029e7ae91b1099b00166
Volodymyr Kopynets <linch433@gmail.com>
2023-04-14 14:46:00 +0300

    Hotfix

--- src/styles/ImageGroup/ImageSolo.jsx
+++ src/styles/ImageGroup/ImageSolo.jsx
@@ -4,14 +4,14 @@ import './imageSoloStyle.css';
 const ImageSolo = ({images}) => {
 
   return (
-    <div className='image_group'>
+    <div className='image_content'>
       {images.map((image, index) => (
-        <div key={index} className={'image_item'}>
+        <div key={index}>
           <div className={'image_source'}>
             <Image src={image.src} alt={image.title} height={'100%'}
                    style={{borderRadius: '5px', objectFit: 'contain'}}/>
           </div>
-          <div className={'image_title'}>{image.title}</div>
+          <div className={'.image_text'}>{image.title}</div>
         </div>
       ))}
     </div>
